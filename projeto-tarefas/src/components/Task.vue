54d6f2057ff413ffe02ae28eedd616014065a35c
lucasbf1992 <lucas.bf@outlook.com>
2022-08-07 11:40:46 -0300

    Commit Inicial

---
+++ projeto-tarefas/src/components/Task.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="task" :class="stateClass">
+        <p>
+            {{ task.name }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+       stateClass() {
+        return {
+            pending: this.task.pending,
+            done: !this.task.pending,
+        }
+       } 
+    }
+}
+</script>
+
+<style scoped>
+
+.task {
+    box-sizing: border-box;
+    width: 350px;
+    height: 150px;
+    border-radius: 8px;
+    font-size: 2rem;
+    font-weight: 300;
+    cursor: pointer;
+    user-select: none;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+.pending {
+    border-left: 12px solid #b73229;
+    background-color: #f44336;
+}
+
+.done {
+    color: #ddd;
+    border-left: 12px solid #4caf50;
+    background-color: #4caf50;
+    text-decoration: line-through;
+}
+
+</style>
